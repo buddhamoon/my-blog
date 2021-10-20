3ee813ed93a01eee372cb7216cae39e1e3cf2cdc
buddhamoon <hy51hy@126.com>
2021-10-20 18:44:04 +0800

    some

--- src/layouts/contentIndex.tsx
+++ src/layouts/contentIndex.tsx
@@ -1,21 +1,32 @@
 import { Layout } from 'antd';
 import styles from './contentIndex.less'
 import NavigationComp from '../components/navigation'
+import { Row, Col } from 'antd'
+import DocList from '@/components/DocList';
 const { Header, Footer, Sider, Content } = Layout;
 
 
 function ContentIndex ( props: any ) {
 
     return <> 
-    <Layout className='styles.dss'>
+    <Layout className={styles.layoutTop}>
       <Header className={styles.headerList}>
         <NavigationComp></NavigationComp>
       </Header>
-      <Layout>
-        <Content>Content</Content>
-        <Sider>Sider</Sider>
+      <Layout className={styles.headerImage}>
+        <Content>
+          
+        </Content>
       </Layout>
-      <Footer>Footer</Footer>
+      <Layout className={styles.contentLayout}>
+        <Content className={styles.contentList}>
+        <DocList></DocList>
+        </Content>
+        <Sider 
+        width={ 350 }
+        className={styles.userSider}>Sider</Sider>
+      </Layout>
+      <Footer className={styles.pageFooter}>Footer</Footer>
     </Layout>
     </>
 }
