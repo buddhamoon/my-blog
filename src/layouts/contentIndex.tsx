dd39c93eddac76787ebd53003a9c960247f74a80
buddhamoon <hy51hy@126.com>
2021-11-16 17:20:44 +0800

    test

--- src/layouts/contentIndex.tsx
+++ src/layouts/contentIndex.tsx
@@ -1,42 +1,36 @@
 import { Layout } from 'antd';
-import styles from './contentIndex.less'
-import NavigationComp from '../components/navigation'
+import styles from './contentIndex.less';
+import NavigationComp from '../components/navigation';
 import { Row, Col } from 'antd';
 import DocList from '@/components/DocList';
 import TitleText from '@/components/TitleText';
 import AboutMeCard from '@/components/AboutMeCard';
 const { Header, Footer, Sider, Content } = Layout;
 
-
-function ContentIndex ( props: any ) {
-
-    return <> 
-    <Layout className={styles.layoutTop}>
-      <Header className={styles.headerList}>
-        <NavigationComp></NavigationComp>
-      </Header>
-      <Layout className={styles.headerImage}>
-        <Content>
-          
-        </Content>
+function ContentIndex(props: any) {
+  return (
+    <>
+      <Layout className={styles.layoutTop}>
+        <Header className={styles.headerList}>
+          <NavigationComp></NavigationComp>
+        </Header>
+        <Layout className={styles.headerImage}>
+          <Content>bolg Text</Content>
+        </Layout>
+        <Layout className={styles.contentLayout}>
+          <Content className={styles.contentList}>
+            <TitleText content="文章列表 - Documnet"></TitleText>
+            <DocList></DocList>
+          </Content>
+          <Sider width={350} className={styles.userSider}>
+            <TitleText content="关于作者 - About"></TitleText>
+            <AboutMeCard src="../assets/50014213.png"></AboutMeCard>
+          </Sider>
+        </Layout>
+        <Footer className={styles.pageFooter}>Footer</Footer>
       </Layout>
-      <Layout className={styles.contentLayout}>
-        <Content className={styles.contentList}>
-          <TitleText content='文章列表 - Documnet'></TitleText>
-        <DocList></DocList>
-        </Content>
-        <Sider 
-        width={ 350 }
-        className={styles.userSider}>
-          <TitleText content='关于作者 - About'></TitleText>
-          <AboutMeCard 
-          src = '../assets/50014213.png'
-          ></AboutMeCard>
-        </Sider>
-      </Layout>
-      <Footer className={styles.pageFooter}>Footer</Footer>
-    </Layout>
     </>
+  );
 }
 
 export default ContentIndex;
